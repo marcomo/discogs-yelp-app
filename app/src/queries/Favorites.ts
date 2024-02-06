dbdf2af64fce99717b92aaa6c7f28030cd904168
Marco Morales <marco.bbcm@gmail.com>
2024-02-06 11:02:13 -0800

    Moves GraphQL query names to consts

--- app/src/queries/Favorites.ts
+++ app/src/queries/Favorites.ts
@@ -1,19 +1,6 @@
-const BusinessFields = (key: string, id: string) => `
-  ${key}: business(id: "${id}") {
-    ...basicBizInfo
-  }
-`
+export const FAVORITES_QUERY_NAME = 'MyFavorites'
 export const FavoritesQuery = `
-    query MyFavorites {
-      ${BusinessFields('yama', 'yama-sushi-and-sake-bar-portland-3')}
-      ${BusinessFields('phoVan', 'pho-van-beaverton-beaverton-5')}
-      ${BusinessFields('laughingPlanet', 'laughing-planet-portland-25')}
-      ${BusinessFields('killerBurger', 'killer-burger-cedar-hills-beaverton-4')}
-      ${BusinessFields('hotLips', 'hotlips-pizza-pearl-portland-2')}
-      ${BusinessFields('nossaFamilia', 'nossa-familia-coffee-portland-3')}
-      ${BusinessFields('lovejoyBakers', 'lovejoy-bakers-portland-2')}
-      ${BusinessFields('sunitas', 'sunitas-thai-kitchen-portland')}
-      ${BusinessFields('courier', 'courier-coffee-roasters-portland')}
+  query ${FAVORITES_QUERY_NAME} {
   }
   fragment basicBizInfo on Business {
     name
