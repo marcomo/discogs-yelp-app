ac4d723b821626b7b70bebd8c81e230512b9ad30
Marco Morales <marco.bbcm@gmail.com>
2024-02-06 11:06:11 -0800

    Updates favorits query aliases
    Uses the alias from the data and converts it to camel case to use as keys in the data

--- app/src/queries/Favorites.ts
+++ app/src/queries/Favorites.ts
@@ -1,6 +1,24 @@
+import toCamelCase from '../utils/toCamelCase'
+
+const businesses = [
+	'yama-sushi-and-sake-bar-portland-3',
+	'pho-van-beaverton-beaverton-5',
+	'laughing-planet-portland-25',
+	'killer-burger-cedar-hills-beaverton-4',
+	'lovejoy-bakers-portland-2',
+	'sunitas-thai-kitchen-portland',
+	'courier-coffee-roasters-portland',
+]
 export const FAVORITES_QUERY_NAME = 'MyFavorites'
 export const FavoritesQuery = `
   query ${FAVORITES_QUERY_NAME} {
+    ${businesses.map(
+			(biz) => `
+        ${toCamelCase(biz)}: business(id: "${biz}") {
+            ...basicBizInfo
+        }
+      `
+		)}
   }
   fragment basicBizInfo on Business {
     name
